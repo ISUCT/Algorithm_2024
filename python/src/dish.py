c728058e22fa6830291c492d26cc57562c1eda6a
sweetfyx <gyrykirill@gmail.com>
2024-02-17 19:35:23 +0300

    lint fix

--- python/src/dish.py
+++ python/src/dish.py
@@ -8,15 +8,15 @@ class Dish:
     @property
     def name(self):
         return self.__name
-    
+
     @property
     def vid(self):
         return self.__vid
-    
+
     @property
     def price(self):
         return self.__price
-    
+
     @name.setter
     def name(self, value):
         self.__name = value
@@ -29,16 +29,13 @@ class Dish:
     def price(self, value):
         self.check_price(value)
         self.__price = value
-        
+
     def check_price(self, price):
         if price >= 5000 or price <= 0:
             raise ValueError("Это блюдо вам не по карману")
-        
+
     @property
     def info(self):
         return f"""Цена: {self.price}
 Название: {self.name}
 Тип блюда: {self.vid}"""
-
-
-        

655fd664027eb097816eb6f827d14c61abc6cd44
sweetfyx <gyrykirill@gmail.com>
2024-02-18 09:37:50 +0300

    another lint fix

--- python/src/dish.py
+++ python/src/dish.py
@@ -1,41 +1,40 @@
 class Dish:
-    def __init__(self, name, vid, price):
+    def __init__(self, name: str, vid: str, price: int):
         self.__name = name
         self.__vid = vid
         self.check_price(price)
         self.__price = price
 
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
-    @property
-    def vid(self):
-        return self.__vid
-
-    @property
-    def price(self):
-        return self.__price
-
     @name.setter
-    def name(self, value):
+    def name(self, value: str):
         self.__name = value
 
+    @property
+    def vid(self) -> str:
+        return self.__vid
+
     @vid.setter
-    def vid(self, value):
+    def vid(self, value: str):
         self.__vid = value
 
+    @property
+    def price(self) -> int:
+        return self.__price
+
     @price.setter
-    def price(self, value):
+    def price(self, value: int):
         self.check_price(value)
         self.__price = value
 
-    def check_price(self, price):
+    def check_price(self, price: int):
         if price >= 5000 or price <= 0:
             raise ValueError("Это блюдо вам не по карману")
 
-    @property
-    def info(self):
+    def info(self) -> str:
         return f"""Цена: {self.price}
 Название: {self.name}
 Тип блюда: {self.vid}"""
--- python/src/main.py
+++ python/src/main.py
@@ -2,4 +2,4 @@ from dish import Dish
 
 if __name__ == "__main__":
     dish = Dish("Борщ", "Суп", 500)
-    print(dish.info)
+    print(dish.info())

53e43f09056f188e73663fbb7f97eb5fb79f53d9
Keezo0 <polinakostina118@gmail.com>
2024-02-18 22:05:20 +0300

    lab6

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,7 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
+import structure
 
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    tom = structure.Employee("Tom", 45, "01.03.2022")
+    michael = structure.Employee("Michael", 20, "14.08.2024")
+    print(tom.print_employee())
+    print(michael.print_employee())

05cb101f74bde6a3c344d6bde5ac6b18aceb84f0
1inHab <alinahabibylina2050@mail.ru>
2024-02-19 07:00:56 +0300

    6 laba - python

---
+++ python/src/__init__.py
@@ -0,0 +1,13 @@
+class Piggy():
+    name = ""
+    age = 0
+    breed = ""
+    sex = ""
+    color = ""
+    
+    def __init__(self, name, age, breed, sex, color) -> None: 
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.sex = sex
+        self.color = color     

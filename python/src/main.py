84c0e27e0782423bd54461f77e0202f42222eb7a
chel <mix.zhuckow2018@yandex.ru>
2024-02-19 21:29:30 +0300

    lab6

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,10 @@
-def summ(a: int, b: int) -> int:
-    return a + b
+import lab6.lab6 as lab6PY
 
+Person1 = lab6PY.Person("Sanya", 18, 60.5, 1.8 )
 
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+Person1.get()
+
+Person2 = lab6PY.Person("Vlad", 21, 50.5, 2.0 )
+
+Person2.get()
+

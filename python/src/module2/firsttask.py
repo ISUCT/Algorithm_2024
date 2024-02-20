dd4144cf4c68433c87efca2241a0908b3fa87b48
MarkVRKS <143934528+MarkVRKS@users.noreply.github.com>
2024-02-20 19:27:34 +0300

    Create firsttask.py

---
+++ python/src/module2/firsttask.py
@@ -0,0 +1,16 @@
+def bubbles(nums):
+    len_nums = len(nums)
+    s = 0
+    for i in range(len_nums - 1):
+        for j in range(len_nums - i - 1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+                s += 1
+                print(*nums)
+    if s == 0:
+        print(0)
+
+N = int(input())
+nums = list(map(int, input().split()))
+bubbles(nums)
+

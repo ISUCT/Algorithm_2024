d640f3d541702b68fd93ceca46a1da384e9675a8
KochetovDanilRomanovich <kocetovdanil677@gmail.com>
2024-02-21 22:50:51 +0300

    лаба 5 питон

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,11 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
+import sample
 
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    print("lab 5")
+    
+    types = ("DOCX", "DOC", "DOTX", "DOT", "RTF", "TXT", "HTM", "PDF", "DOCM", "DOTM", "XML", "MHT", "DIC", "THMX")
+    documentik = sample.Document(type_of_document="docx", size_of_document=300, title_of_document="failik", types=types)
+    
+    print("The document type is {0}".format(documentik.type_of_document))
+    print("The document size is {0} bites".format(documentik.size_of_document))
+    print("The document title is {0}".format(documentik.title_of_document))

f154e0ddb8821341481c70d9ad6e4a983ae60c20
Olga Fedorova <olefiodorowa@yandex.ru>
2024-02-21 21:49:33 +0300

    lab5 Fedorova

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,5 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
-
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+from lab5 import Mouse
+Mouse1 = Mouse(2, "чёрный", "Вероника", "женский")
+Mouse2 = Mouse(1, "белый", "Оля", "женский")
+print("Возраст мыши:", Mouse1.age, "\n", "Цвет мыши:", Mouse1.color,"\n", 
+      "Имя мыши:", Mouse1.name, "\n", "Пол:", Mouse1.gender, "\n")

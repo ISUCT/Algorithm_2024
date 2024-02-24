31046b9a9b072a77eed3d4d9fa2d07f1bd86277c
__HoLeN__ <holenyt@gmail.com>
2024-02-24 18:26:11 +0300

    fix lint

--- python/src/lab_struct.py
+++ python/src/lab_struct.py
@@ -3,37 +3,36 @@ class Worker:
         self.__name: str = name
         self.__status: str = status
         self.__salary: int = checkSalary(salary_worker)
-
-
+    
     def __del__(self):
         print(f"{self.__name} был удалён")
 
-
     def __str__(self) -> str:
-        return(f'Имя: {self.__name}, Статус: {self.__status}, Зарплата: {self.__salary}')
-
+        return (f'Имя {self.__name} Статус {self.__status} Зп {self.__salary}')
 
     def come(self):
         print(f"{self.__name} пришёл на работу.")
 
-
     @property
     def name(self) -> str:
         return self.__name
+
     @property
     def status(self) -> str:
         return self.__status
+
     @property
     def salary(self) -> int:
         return self.__salary
 
-
     @name.setter
     def name(self, name: str):
         self.__name: str = name
+
     @status.setter
     def status(self, status: str):
         self.__status: str = status
+
     @salary.setter
     def salary(self, salary: int):
         self.__salary: int = checkSalary(salary)
@@ -41,8 +40,8 @@ class Worker:
 
 def checkSalary(salary: int) -> int:
     try:
-        if (salary>=15000):
+        if (salary >= 15000):
             return salary
-        raise ValueError("Ошибка: Минимальная зарплата 15000");
+        raise ValueError("Ошибка: Минимальная зарплата 15000")
     except ValueError as error:
-        print(str(error));
+        print(str(error))

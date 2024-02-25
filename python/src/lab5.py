38b62256fc8d74f4cb5cac8b43a6b2cf63b93513
J0es1ick <mrjoeslick@gmail.com>
2024-02-25 18:28:02 +0300

    lab5

---
+++ python/src/lab5.py
@@ -0,0 +1,29 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self._name = name    
+        self._age = self.set_age(age)   
+        self._gender = gender
+    
+    def get_name(self):
+        return self._name
+    
+    def get_age(self):
+        return self._age
+    
+    def get_gender(self):
+        return self._gender
+    
+    def set_age(self, age):
+        if age > 0 and age < 122:
+            self._age = age
+        else:
+            raise Exception("age is higher or lower than bound")
+        
+    def know_gender(self):
+        return(self._name + "'s gender - " + self._gender)
+	    
+    def go_to_walk(self, time):
+        if time <= 12 and time >= 0:
+            return("At " + str(time) + " o'clock " + self._name + " goes for a walk.")
+        else:
+            raise Exception("time can't be more than 12 or less than 0")
--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,19 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
+import lab5
 
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    john = lab5.Person("John", 22, "male")
+    michael = lab5.Person("Michael", 34, "male")
+    
+    try:
+        john.set_age(25)
+    except Exception as error:
+        print("have one error:", error)
+    
+    print(michael.know_gender())
+    
+    try:
+        print(michael.go_to_walk(10))
+    except Exception as error:
+        print("have one error:", error)
+        
+    print(john.get_name() + "'s age - " + str(john.get_age()))

7df66b6f7ce55cf2dcc2bc446331c5fc0c8bdbd0
LysovDmitri <lysovdmitriy37@gmsil.com>
2024-02-25 23:12:52 +0300

    lab1

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,10 @@
-def summ(a: int, b: int) -> int:
-    return a + b
+from __init__ import Person
 
-
-if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+if __name__ == '__main__':
+    human_1 = Person(37, 'Сергей', 'муж')
+    human_1.set_name('Константин')
+    human_1.set_age(40)
+    print(human_1.get_name(), human_1.get_age())
+    human_2 = Person(18, 'Ульяна', 'жен')
+    print(human_2.get_name(), human_2.get_age())
+    human_2.death_person()

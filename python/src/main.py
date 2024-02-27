5b4eb195a4a0e7b272764b51725d4fb6001938a9
pvzprrrr <alenanaumovvva@mail.ru>
2024-02-27 19:50:09 +0300

    laba6

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,13 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
-
+import lab
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    bludo_1 = lab.Dish(430, 300, "Borsch")
+    bludo_1.set_name("Kartoshka")
+    bludo_1.set_calorie(200)
+    bludo_1.set_price(134)
+    bludo_2 = lab.Dish(200, 345, "Olivie")
+    bludo_2.set_name("Kraboviy salat")
+    bludo_2.set_calorie(320)
+    bludo_2.set_price(150)
+    bludo_1.print_bludo()
+    bludo_2.print_bludo()
+    

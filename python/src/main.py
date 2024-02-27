1427f312e286a00dff7b04e042a84c0afde4c948
sps35 <spiridonovapolina5@gmail.com>
2024-02-27 09:57:46 +0300

    lab6 fixes

--- python/src/main.py
+++ python/src/main.py
@@ -2,8 +2,8 @@ import lab6
 if __name__ == "__main__":
     print("struct Foxes")
     foxy = lab6.Fox("Foxy", 5 ,"red")
+    foxy.new_fox()
     try:
         foxy.set_age(5)
     except Exception as error:
         print("have one error", error)
-    foxy.new_fox()

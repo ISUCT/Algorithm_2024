3b5939e146aa28e71a35895ee0528edcd79275aa
Max1m1l1on <145985997+Max1m1l1on@users.noreply.github.com>
2024-02-29 14:54:42 +0300

    Lab 5 piton

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,10 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
+import sample
 
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    print("lab 5 python")
+    sobachka = sample.Dog(age=12, name="Zhychka", gender="male")
+
+    print(f"The dog's name is {sobachka.name}")
+    print(f"The dog {sobachka.name} is {sobachka.age} years old")
+    print(f"{sobachka.name} is a {sobachka.gender}")
+    

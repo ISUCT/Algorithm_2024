a96cbae47ef0a0dadb0f6146dbbb695a429e5bb1
smirnovg2005 <smirnovg2005@gmail.com>
2024-03-11 14:58:18 +0300

    Lab 5 to Python

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,22 @@
-def summ(a: int, b: int) -> int:
-    return a + b
-
+from structure import klass
 
 if __name__ == "__main__":
-    print("Hello world")
-    print(summ(3, 4))
+    print(f"{klass.Lincore.length}")
+    print(f"{klass.Lincore.name}")
+
+    Korabl_2 = klass.ship("K2", -130.4, 40.8, 100)
+    klass.ship.output(Korabl_2)
+    print(f"{Korabl_2.name}")
+    klass.ship.output(klass.Korabl_3)
+
+    klass.ship.Sail(klass.Lincore)
+
+    klass.ship.output(Korabl_2)
+
+    klass.ship.output(klass.Lincore)
+
+    klass.Lincore.length = -100
+    print(klass.Lincore.name)
+
+    print(f"{klass.Lincore.name}")
+    print(f"{klass.Lincore.length}")
